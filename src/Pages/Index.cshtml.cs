ca4f1b1db948c0c7e3199ba08877cefc62173edb
rama krishna p.v <pvrk007@gmail.com>
2022-04-11 19:29:15 -0700

    Initial check-in of module ContosoCrafts.WebSite

---
+++ src/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+
+    /// <summary>
+    /// Mike Koenig
+    /// </summary>
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+        public IEnumerable<Product>? Products { get; private set; }
+
+        public void OnGet() => Products = ProductService.GetProducts();
+    }
+}
